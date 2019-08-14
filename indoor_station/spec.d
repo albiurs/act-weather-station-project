cacddf3fca242d7c0d8323216f426c37ea47a3e6
Urs Albisser <u.albisser@gmail.com>
2019-08-14 10:39:24 +0200

    split projects into Arduino Uno vs. Arduino Mega projects

--- indoor_station/spec.d
+++
@@ -1,2 +0,0 @@
-spec.o: \
- /Users/alu/Documents/act-weather-station-project/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
---
+++ outdoor_station_uno/spec.d
@@ -0,0 +1,2 @@
+spec.o: \
+ /Users/alu/Documents/sloeber-workspaces/act-weather-station-project/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
