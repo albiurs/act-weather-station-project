3df47333920dea2a9e2632c7e69f9687b12aa587
Urs Albisser <u.albisser@gmail.com>
2019-08-18 14:38:50 +0200

    indoor-mega: remove GFX demo and update comments; outdoor-mega: sketch setup with DHT22 and nRF24L01+ Radio TX support

--- indoor_station_mega2560/indoor_station_mega2560.ino
+++ indoor_station_mega2560/indoor_station_mega2560.ino
@@ -13,6 +13,10 @@
  *	- NRFLite					2.2.2	nRF24L01+ 2.4 GHz Transceiver library: https://github.com/dparson55/NRFLite
  */
 
+
+/*
+ * Include libraries
+ */
 #include "Arduino.h"
 #include "DHT.h"				// DHT sensor library
 #include <DS3231.h>				// RTC DS3231 library
@@ -23,7 +27,9 @@
 #include <NRFLite.h>			// nRF24L01+ Transceiver library
 
 
-// Debug Sketch
+/*
+ * Debug initialize
+ */
 #define DEBUG // activate "debug switch"
 #ifdef DEBUG
 #define DBUG_PRINT(x) Serial.print(x)		// activate print
@@ -34,14 +40,18 @@
 #endif
 
 
-// DHT
+/*
+ * DHT initialize
+ */
 #define DHTPIN 8			// Digital pin connected to the DHT sensor
 #define DHTTYPE DHT22		// DHT type = DHT22
 
 DHT dht(DHTPIN, DHTTYPE);	// Initialize DHT sensor.
 
 
-// RTC DS3231
+/*
+ * RTC DS3231 initialize
+ */
 DS3231 Clock;		// initialize DS3231 RTC
 bool Century=false;
 bool h12;
@@ -52,7 +62,9 @@ bool ADy, A12h, Apm;
 byte year, month, date, DoW, hour, minute, second;
 
 
-// Adafruit 1.5" 16-bit Color OLED
+/*
+ * Adafruit 1.5" 16-bit Color OLED Initialize
+ */
 // Screen dimensions:
 #define SCREEN_WIDTH  128
 #define SCREEN_HEIGHT 128 // Change this to 96 for 1.27" OLED.
@@ -82,11 +94,10 @@ Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, CS_PIN, DC_
 // to use the microSD card (see the image drawing example)
 //Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, CS_PIN, DC_PIN, RST_PIN);
 
-float p = 3.1415926;	// pi required for graphic demo
 
-
-// nRF24L01+ Radio
-// Read through 'NRFLite.h' for a description of all the methods available in the library.
+/*
+ * nRF24L01+ Radio RX initialize
+ */
 
 // Radio    Arduino
 // CE    -> 9
@@ -121,20 +132,30 @@ RadioPacket _radioData;
 void setup()
 {
 
-	// config serial port & serial monitor to Baud rate 9600 bits/s
+	/*
+	 * Start serial port & serial monitor to Baud rate 9600 bits/s
+	 */
 	Serial.begin(9600); // serial return of Arduino's output
 	//DEBUG_PRINTLINE("my debug message");
 
 
-	// Start the I2C interface
+	/*
+	 * Start the I2C interface
+	 */
 	DEBUG_PRINTLINE("Start the I2C interface");
 	Wire.begin();
 
 
-	// Start DHT measurement
+	/*
+	 * Start DHT measurement
+	 */
 	DEBUG_PRINTLINE(F("Start DHT measurement"));
 	dht.begin();
 
+
+	/*
+	 * Start RTC > setup date & time
+	 */
 	// VERYIMPORTANT COMMENTS BELOW!!!
 	// If you want to set the time, change these numbers to the date and time
 	// you want to set to, and then upload it to the arduino.
@@ -152,7 +173,9 @@ void setup()
 	Clock.setYear(19);  //Set the year (Last two digits of the year)
 
 
-	// Start OLED
+	/*
+	 * Start OLED
+	 */
 	tft.begin();
 
 	// You can optionally rotate the display by running the line below.
@@ -230,6 +253,7 @@ void loop()
 	Serial.println();
 	Serial.println();
 
+	// Print values to OLED
 	tft.fillScreen(BLACK);
 	tft.setCursor(0, 5);
 	tft.setTextColor(BLUE);
@@ -308,11 +332,11 @@ void loop()
 	Serial.println();
 	Serial.println();
 
-	// New line on display
+	// New line on console
 	Serial.print('\n');
-
-	// Print Date & Time
 	delay(1500);
+
+	// Print values to OLED
 	tft.setCursor(0, 5);
 	tft.fillScreen(BLACK);
 	tft.setTextColor(WHITE);
@@ -351,7 +375,20 @@ void loop()
 		msg += " Failed TX";
 
 		Serial.println(msg);
+
+		// Print values to OLED
+		tft.fillScreen(BLACK);
+		tft.setCursor(0, 5);
+		tft.setTextColor(RED);
+		tft.setTextSize(2);
+		tft.println("Outdoor RX");
+
+		tft.setTextColor(BLUE);
+		tft.setTextSize(2);
+		tft.println(msg);
 	}
+
+	delay(2000);
 
 }
 
@@ -361,200 +398,6 @@ void loop()
 /*************
  * FUNCTIONS *
  ************/
-
-/*
- * OLED Color Display Functions
- */
-void testlines(uint16_t color) {
-	tft.fillScreen(BLACK);
-	for (uint16_t x=0; x < tft.width()-1; x+=6) {
-		tft.drawLine(0, 0, x, tft.height()-1, color);
-	}
-	for (uint16_t y=0; y < tft.height()-1; y+=6) {
-		tft.drawLine(0, 0, tft.width()-1, y, color);
-	}
-
-	tft.fillScreen(BLACK);
-	for (uint16_t x=0; x < tft.width()-1; x+=6) {
-		tft.drawLine(tft.width()-1, 0, x, tft.height()-1, color);
-	}
-	for (uint16_t y=0; y < tft.height()-1; y+=6) {
-		tft.drawLine(tft.width()-1, 0, 0, y, color);
-	}
-
-	tft.fillScreen(BLACK);
-	for (uint16_t x=0; x < tft.width()-1; x+=6) {
-		tft.drawLine(0, tft.height()-1, x, 0, color);
-	}
-	for (uint16_t y=0; y < tft.height()-1; y+=6) {
-		tft.drawLine(0, tft.height()-1, tft.width()-1, y, color);
-	}
-
-	tft.fillScreen(BLACK);
-	for (uint16_t x=0; x < tft.width()-1; x+=6) {
-		tft.drawLine(tft.width()-1, tft.height()-1, x, 0, color);
-	}
-	for (uint16_t y=0; y < tft.height()-1; y+=6) {
-		tft.drawLine(tft.width()-1, tft.height()-1, 0, y, color);
-	}
-
-}
-
-void testdrawtext(char *text, uint16_t color) {
-	tft.setCursor(0,0);
-	tft.setTextColor(color);
-	tft.print(text);
-}
-
-void testfastlines(uint16_t color1, uint16_t color2) {
-	tft.fillScreen(BLACK);
-	for (uint16_t y=0; y < tft.height()-1; y+=5) {
-		tft.drawFastHLine(0, y, tft.width()-1, color1);
-	}
-	for (uint16_t x=0; x < tft.width()-1; x+=5) {
-		tft.drawFastVLine(x, 0, tft.height()-1, color2);
-	}
-}
-
-void testdrawrects(uint16_t color) {
-	tft.fillScreen(BLACK);
-	for (uint16_t x=0; x < tft.height()-1; x+=6) {
-		tft.drawRect((tft.width()-1)/2 -x/2, (tft.height()-1)/2 -x/2 , x, x, color);
-	}
-}
-
-void testfillrects(uint16_t color1, uint16_t color2) {
-	tft.fillScreen(BLACK);
-	for (uint16_t x=tft.height()-1; x > 6; x-=6) {
-		tft.fillRect((tft.width()-1)/2 -x/2, (tft.height()-1)/2 -x/2 , x, x, color1);
-		tft.drawRect((tft.width()-1)/2 -x/2, (tft.height()-1)/2 -x/2 , x, x, color2);
-	}
-}
-
-void testfillcircles(uint8_t radius, uint16_t color) {
-	for (uint8_t x=radius; x < tft.width()-1; x+=radius*2) {
-		for (uint8_t y=radius; y < tft.height()-1; y+=radius*2) {
-			tft.fillCircle(x, y, radius, color);
-		}
-	}
-}
-
-void testdrawcircles(uint8_t radius, uint16_t color) {
-	for (uint8_t x=0; x < tft.width()-1+radius; x+=radius*2) {
-		for (uint8_t y=0; y < tft.height()-1+radius; y+=radius*2) {
-			tft.drawCircle(x, y, radius, color);
-		}
-	}
-}
-
-void testtriangles() {
-	tft.fillScreen(BLACK);
-	int color = 0xF800;
-	int t;
-	int w = tft.width()/2;
-	int x = tft.height();
-	int y = 0;
-	int z = tft.width();
-	for(t = 0 ; t <= 15; t+=1) {
-		tft.drawTriangle(w, y, y, x, z, x, color);
-		x-=4;
-		y+=4;
-		z-=4;
-		color+=100;
-	}
-}
-
-void testroundrects() {
-	tft.fillScreen(BLACK);
-	int color = 100;
-
-	int x = 0;
-	int y = 0;
-	int w = tft.width();
-	int h = tft.height();
-	for(int i = 0 ; i <= 24; i++) {
-		tft.drawRoundRect(x, y, w, h, 5, color);
-		x+=2;
-		y+=3;
-		w-=4;
-		h-=6;
-		color+=1100;
-		Serial.println(i);
-	}
-}
-
-void tftPrintTest() {
-	tft.fillScreen(BLACK);
-	tft.setCursor(0, 5);
-	tft.setTextColor(RED);
-	tft.setTextSize(1);
-	tft.println("Hello World!");
-	tft.setTextColor(YELLOW);
-	tft.setTextSize(2);
-	tft.println("Hello World!");
-	tft.setTextColor(BLUE);
-	tft.setTextSize(3);
-	tft.print(1234.567);
-	delay(1500);
-	tft.setCursor(0, 5);
-	tft.fillScreen(BLACK);
-	tft.setTextColor(WHITE);
-	tft.setTextSize(0);
-	tft.println("Hello World!");
-	tft.setTextSize(1);
-	tft.setTextColor(GREEN);
-	tft.print(p, 6);
-	tft.println(" Want pi?");
-	tft.println(" ");
-	tft.print(8675309, HEX); // print 8,675,309 out in HEX!
-	tft.println(" Print HEX!");
-	tft.println(" ");
-	tft.setTextColor(WHITE);
-	tft.println("Sketch has been");
-	tft.println("running for: ");
-	tft.setTextColor(MAGENTA);
-	tft.print(millis() / 1000);
-	tft.setTextColor(WHITE);
-	tft.print(" seconds.");
-}
-
-void mediabuttons() {
-	// play
-	tft.fillScreen(BLACK);
-	tft.fillRoundRect(25, 10, 78, 60, 8, WHITE);
-	tft.fillTriangle(42, 20, 42, 60, 90, 40, RED);
-	delay(500);
-	// pause
-	tft.fillRoundRect(25, 90, 78, 60, 8, WHITE);
-	tft.fillRoundRect(39, 98, 20, 45, 5, GREEN);
-	tft.fillRoundRect(69, 98, 20, 45, 5, GREEN);
-	delay(500);
-	// play color
-	tft.fillTriangle(42, 20, 42, 60, 90, 40, BLUE);
-	delay(50);
-	// pause color
-	tft.fillRoundRect(39, 98, 20, 45, 5, RED);
-	tft.fillRoundRect(69, 98, 20, 45, 5, RED);
-	// play color
-	tft.fillTriangle(42, 20, 42, 60, 90, 40, GREEN);
-}
-
-/**************************************************************************/
-/*!
-    @brief  Renders a simple test pattern on the screen
- */
-/**************************************************************************/
-void lcdTestPattern(void)
-{
-	static const uint16_t PROGMEM colors[] =
-	{ RED, YELLOW, GREEN, CYAN, BLUE, MAGENTA, BLACK, WHITE };
-
-	for(uint8_t c=0; c<8; c++) {
-		tft.fillRect(0, tft.height() * c / 8, tft.width(), tft.height() / 8,
-				pgm_read_word(&colors[c]));
-	}
-}
-
 
 /*
  * Debug functions
--- outdoor_station_mega2560/outdoor_station_mega2560.ino
+++ outdoor_station_mega2560/outdoor_station_mega2560.ino
@@ -1,12 +1,220 @@
+/*
+ * Arduino Outdoor Weather Station Project
+ *
+ * REQUIRES the following Arduino libraries:
+ * 	- DHT_sensor_library		v1.3.7	DHT22 Sensor: https://github.com/adafruit/DHT-sensor-library
+ * 	- Adafruit_Unified_Sensor	v1.0.3	Adafruit Unified Sensor: https://github.com/adafruit/Adafruit_Sensor
+ *	- SPI						------	SPI Master library for Arduino (Arduino built-in)
+ *	- NRFLite					2.2.2	nRF24L01+ 2.4 GHz Transceiver library: https://github.com/dparson55/NRFLite
+ */
+
+
+/*
+ * Include libraries
+ */
 #include "Arduino.h"
-//The setup function is called once at startup of the sketch
+#include "DHT.h"				// DHT sensor librar
+#include <SPI.h>				// SPI Master library for Arduino (required by Adafruit_GFX.h and NRFLite.h)
+#include <NRFLite.h>			// nRF24L01+ Transceiver library
+
+
+/*
+ * Debug initialize
+ */
+#define DEBUG // activate "debug switch"
+#ifdef DEBUG
+#define DBUG_PRINT(x) Serial.print(x)		// activate print
+#define DEBUG_PRINTLINE(x) Serial.println(x)	// activate print line
+#else
+#define DEBUG_PRINT(x)						// do nothing
+#define DEBUG_PRINTLINE(x)					// do nothing
+#endif
+
+
+/*
+ * DHT initialize
+ */
+#define DHTPIN 3			// Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22		// DHT type = DHT22
+
+DHT dht(DHTPIN, DHTTYPE);	// Initialize DHT sensor.
+
+
+/*
+ * nRF24L01+ Radio TX initialize
+ */
+
+// Radio    Arduino
+// CE    -> 1
+// CSN   -> 2	(Hardware SPI SS)
+// MOSI  -> 11	(Hardware SPI MOSI)
+// MISO  -> 12	(Hardware SPI MISO)
+// SCK   -> 13	(Hardware SPI SCK)
+// IRQ   -> No	connection
+// VCC   -> No	more than 3.6 volts
+// GND   -> GND
+
+const static uint8_t RADIO_ID = 1;             // Our radio's id.
+const static uint8_t DESTINATION_RADIO_ID = 0; // Id of the radio we will transmit to.
+const static uint8_t PIN_RADIO_CE = 1;
+const static uint8_t PIN_RADIO_CSN = 2;
+
+struct RadioPacket // Any packet up to 32 bytes can be sent.
+{
+	uint8_t FromRadioId;
+	uint32_t OnTimeMillis;
+	uint32_t FailedTxCount;
+};
+
+NRFLite _radio;
+RadioPacket _radioData;
+
+
+
+
+/*****************
+ * ARDUINO SETUP *
+ ****************/
 void setup()
 {
-// Add your initialization code here
+
+	/*
+	 * Start serial port & serial monitor to Baud rate 9600 bits/s
+	 */
+	Serial.begin(9600); // serial return of Arduino's output
+	//DEBUG_PRINTLINE("my debug message");
+
+
+	/*
+	 * Start DHT measurement
+	 */
+	DEBUG_PRINTLINE(F("Start DHT measurement"));
+	dht.begin();
+
+
+	/*
+	 * Start nRF24L01+ Radio
+	 */
+	// By default, 'init' configures the radio to use a 2MBPS bitrate on channel 100 (channels 0-125 are valid).
+	// Both the RX and TX radios must have the same bitrate and channel to communicate with each other.
+	// You can run the 'ChannelScanner' example to help select the best channel for your environment.
+	// You can assign a different bitrate and channel as shown below.
+	//   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE250KBPS, 0)
+	//   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE1MBPS, 75)
+	_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE2MBPS, 100); // THE DEFAULT
+
+	if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN))
+	{
+		Serial.println("Cannot communicate with radio");
+		while (1); // Wait here forever.
+	}
+
+	_radioData.FromRadioId = RADIO_ID;
+
 }
 
-// The loop function is called in an endless loop
+
+
+
+/****************
+ * ARDUINO LOOP *
+ ***************/
 void loop()
 {
-//Add your repeated code here
+
+	/*
+	 * DHT Sensor read
+	 */
+	// Wait a few seconds between measurements.
+	delay(3000);
+
+	// Reading temperature or humidity takes about 250 milliseconds!
+	// Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+	float h = dht.readHumidity();
+	// Read temperature as Celsius (the default)
+	float t = dht.readTemperature();
+	// Read temperature as Fahrenheit (isFahrenheit = true)
+	float f = dht.readTemperature(true);
+
+	// Check if any reads failed and exit early (to try again).
+	if (isnan(h) || isnan(t) || isnan(f)) {
+		Serial.println(F("Failed to read from DHT sensor!"));
+		return;
+	}
+
+	// Compute heat index in Fahrenheit (the default)
+	//float hif = dht.computeHeatIndex(f, h);
+	// Compute heat index in Celsius (isFahreheit = false)
+	float hic = dht.computeHeatIndex(t, h, false);
+
+	Serial.print(F("Humidity: "));
+	Serial.print(h);
+	Serial.print(F("%  Temperature: "));
+	Serial.print(t);
+	Serial.print(F("°C "));
+	//Serial.print(f);
+	//Serial.print(F("°F"));
+	Serial.print(F("  Heat index: "));
+	Serial.print(hic);
+	Serial.println(F("°C "));
+	//Serial.print(hif);
+	//Serial.println(F("°F"));
+	Serial.println();
+	Serial.println();
+
+
+	/*
+	 * nRF24L01+ Radio RX
+	 */
+	_radioData.OnTimeMillis = millis();
+
+	Serial.print("Sending ");
+	Serial.print(_radioData.OnTimeMillis);
+	Serial.print(" ms");
+
+	// By default, 'send' transmits data and waits for an acknowledgement.  If no acknowledgement is received,
+	// it will try again up to 16 times.  You can also perform a NO_ACK send that does not request an acknowledgement.
+	// The data packet will only be transmitted a single time so there is no guarantee it will be successful.  Any random
+	// electromagnetic interference can sporatically cause packets to be lost, so NO_ACK sends are only suited for certain
+	// types of situations, such as streaming real-time data where performance is more important than reliability.
+	//   _radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData), NRFLite::NO_ACK)
+	_radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData), NRFLite::REQUIRE_ACK); // THE DEFAULT
+
+	if (_radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData))) // Note how '&' must be placed in front of the variable name.
+	{
+		Serial.println("...Success");
+	}
+	else
+	{
+		Serial.println("...Failed");
+		_radioData.FailedTxCount++;
+	}
+
+	delay(1000);
+
+
+}
+
+
+
+
+/*************
+ * FUNCTIONS *
+ ************/
+
+/*
+ * Debug functions
+ */
+void print (const char str[], int number) {
+	char buf[100];
+	sprintf(buf, "%s %d", str, number);
+	DEBUG_PRINTLN(buf);
+}
+
+void print (const char str[], float number) {
+	char buf[100];
+	char strbuf[10];
+	dtostrf(number, 3, 3, strbuf);
+	sprintf(buf, "%s %s", str, strbuf);
+	DEBUG_PRINTLN(buf);
 }
